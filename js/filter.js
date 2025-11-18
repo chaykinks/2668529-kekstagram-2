c7bdefe6514c25ef602e20d1e0a62fb65e1d943e
chaykinks <chaykinks@gmail.com>
2025-11-18 18:50:55 +0500

    исправил именование массивов на множественное число

--- js/filter.js
+++ js/filter.js
@@ -2,7 +2,7 @@ import { debounce } from './util.js';
 import { clearPictures, renderPictures, getAllPictures } from './pictures.js';
 
 const filters = document.querySelector('.img-filters');
-const filterButton = filters.querySelectorAll('.img-filters__button');
+const filterButtons = filters.querySelectorAll('.img-filters__button');
 
 const RANDOM_PICTURES_COUNT = 10;
 const RERENDER_DELAY = 500;
@@ -13,8 +13,8 @@ const FILTER = {
   discussed: 'filter-discussed',
 };
 
-const getRandomUniquePictures = (array, count) => {
-  const shuffled = [...array].sort(() => Math.random() - 0.5);
+const getRandomUniquePictures = (pictures, count) => {
+  const shuffled = [...pictures].sort(() => Math.random() - 0.5);
   return shuffled.slice(0, count);
 };
 
@@ -45,7 +45,7 @@ const setFilterHandlers = () => {
     if (!button) {
       return;
     }
-    filterButton.forEach((btn) => btn.classList.remove('img-filters__button--active'));
+    filterButtons.forEach((btn) => btn.classList.remove('img-filters__button--active'));
     button.classList.add('img-filters__button--active');
     debouncedFilter(button.id);
   });
